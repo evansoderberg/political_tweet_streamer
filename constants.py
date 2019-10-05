94777877741fe53a52acac42ef7ae5b2d63eb74a
Evan Soderberg <evansoderberg@gmail.com>
2019-10-05 15:51:30 -0700

    Integrate segment, update to current 2020 democratic candidates

--- constants.py
+++ constants.py
@@ -2,8 +2,18 @@ import os
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 CANDIDATES = {
-    'sanders': r'bernie sanders',
-    'trump': r'donald trump',
-    'cruz': r'ted cruz',
-    'clinton': r'hillary clinton'
+    'sanders': r'(Bernie Sanders|@SenSanders)',
+    'warren': r'(Elizabeth Warren|@ewarren)',
+    'harris': r'(Kamala Harris|@KamalaHarris)',
+    'biden': r'(Joe Biden|@JoeBiden)',
+    'booker': r'(Cory Booker|@CoryBooker)',
+    'buttigieg': r'(Pete Buttigieg|@PeteButtigieg)',
+    'castro': r'(Julián Castro|Julian Castro|@JulianCastro)',
+    'gabbard': r'(Tulsi Gabbard|@TulsiGabbard)',
+    'klobuchar': r'(Amy Klobuchar|@amyklobuchar)',
+    'orourke': r"(Beto O’Rourke|Beto ORourke|Beto O'Rourke|@BetoORourke)",
+    'steyer': r'(Tom Steyer|@TomSteyer)',
+    'yang': r'(Andrew Yang|@AndrewYang)'
 }
+
+RETWEET = 'RT'
--- stream.py
+++ stream.py
@@ -1,5 +1,5 @@
 '''
-Stream tweets about 2016 US presidential candidates
+Stream tweets about 2020 Democratic presidential candidates
 
 Inspiration from:
 Adil Moujahid
@@ -9,13 +9,16 @@ Reference: http://adilmoujahid.com/posts/2014/07/twitter-analytics/
 import os
 import re
 import json
+import analytics
 
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
-from secrets import API_KEY, API_SECRET, ACCESS_TOKEN, TOKEN_SECRET
-from constants import CANDIDATES, PROJECT_ROOT
+from secrets import API_KEY, API_SECRET, ACCESS_TOKEN, TOKEN_SECRET, SEGMENT_WRITE_KEY
+from constants import CANDIDATES, PROJECT_ROOT, RETWEET
+
+analytics.write_key = SEGMENT_WRITE_KEY
 
 
 class StdOutListener(StreamListener):
@@ -23,11 +26,9 @@ class StdOutListener(StreamListener):
     Tweet listener that does some very basic data cleaning
     '''
 
-    def write(self, tweet):
-        # file_name = 'tweets.json'
+    def _write_tweet(self, tweet):
         path = os.getcwd()
         file_loc = '{0}/data/tweets.json'.format(path) 
-        print file_loc
         output = json.dumps(tweet) + ',\n'
         mode = 'w'
         if os.path.isfile(file_loc):
@@ -36,42 +37,50 @@ class StdOutListener(StreamListener):
         with open(file_loc, mode) as json_file:
             json_file.write(output)
 
-    def get_candidate(self, tweet):
+        analytics.track('1', tweet['candidate'], tweet)
+
+    def _get_candidate(self, tweet):
         text = tweet.get('text')
         # assume the first matched candidate is the ONLY candidate
         # discussed in the tweet for simplicity
-        for key, value in CANDIDATES.iteritems():
-            candidate = CANDIDATES[key]
-            if re.search(candidate, text, re.IGNORECASE):
+        for key, value in CANDIDATES.items():
+            candidate = re.compile(CANDIDATES[key], flags=re.IGNORECASE)
+            if re.search(candidate, text):
                 return key
         return None
 
-    def clean(self, data):
+    def _clean(self, data):
         parsed = json.loads(data)
         tweet = dict((k, parsed[k]) for k in ('id', 'text', 'user', 'created_at'))
         return tweet
 
-    def on_data(self, data):
-        tweet = self.clean(data)
-        candidate = self.get_candidate(tweet)
-        if candidate:
-            tweet['candidate'] = candidate
-            print "Write tweet for candidate: {0}".format(CANDIDATES[candidate])
-            self.write(tweet)
-        else:
-            print 'notta'
+    def _is_retweet(self, tweet):
+        return tweet['text'].startswith(RETWEET)
 
-        return True
+    def on_data(self, data):
+        tweet = self._clean(data)
+        candidate = self._get_candidate(tweet)
+        is_retweet = self._is_retweet(tweet)
+        if candidate and not is_retweet:
+            tweet['candidate'] = candidate
+            print("Write tweet for candidate: {0}".format(candidate))
+            self._write_tweet(tweet)
 
     def on_error(self, status):
-        print status
+        print('StreamListener error: ', status)
 
+def _get_track_list():
+    track_strings = []
+    for value in CANDIDATES.values():
+        matches = re.sub('[()]', '', value)
+        matches = matches.split('|')
+        track_strings += matches
+    return track_strings
 
 if __name__ == '__main__':
-
     listener = StdOutListener()
     auth = OAuthHandler(API_KEY, API_SECRET)
     auth.set_access_token(ACCESS_TOKEN, TOKEN_SECRET)
     stream = Stream(auth, listener)
-    stream.filter(track=CANDIDATES.values())
+    stream.filter(track=_get_track_list())
 
